8c08fc1673bc34d4baf6aec05a3f950e5e415021
Trung Le <le.hg.trung@gmail.com>
2022-12-15 05:18:05 -0700

    remove format iteration

--- methods/tri_training/tri_training.py
+++ methods/tri_training/tri_training.py
@@ -67,7 +67,7 @@ def tri_training(labeled_path,
         labeled_paths.append(add_suffix_to_path(labeled_path, suffix=i, split_by='.'))
         labeled_model_paths.append(add_suffix_to_path(labeled_model_path, suffix=i, split_by=''))
         prediction_paths.append(add_suffix_to_path(prediction_path, suffix=i, split_by='.'))
-        test_prediction_paths.append(add_suffix_to_path(prediction_path, suffix=i, split_by='.'))
+        test_prediction_paths.append(add_suffix_to_path(test_prediction_path, suffix=i, split_by='.'))
         selection_paths.append(add_suffix_to_path(selection_path, suffix=i, split_by='.'))
         train_log_paths.append(add_suffix_to_path(train_log_path, suffix=i, split_by=''))
         agreement_paths.append(add_suffix_to_path(agreement_path, suffix=i, split_by='.'))
@@ -97,10 +97,27 @@ def tri_training(labeled_path,
     else:
         logger.info('Labeled model exists, skip training ...')
 
+    # Make prediction for each model on test data at round -1
+    for i in range(3):
+        modify_config_file(DEFAULT_PREDICT_CONFIG_PATH,
+                           TEMP_PREDICT_CONFIG_PATH,
+                           {
+                               'model_path': os.path.join(labeled_model_paths[i]
+                                                          .format(-1), 'final_model'),
+                               'dataset_path': DEFAULT_TEST_PATH,
+                               'predictions_path': test_prediction_paths[i]
+                           })
+        script = PREDICT_SCRIPT.format(config_path=TEMP_PREDICT_CONFIG_PATH)
+        logger.info(f'Round #{-1}: Predict on test data on model {i}')
+        subprocess.run(script, shell=True, check=True)
+    evaluate_tri_training(DEFAULT_TEST_PATH,
+                          test_prediction_paths[0],
+                          test_prediction_paths[1],
+                          test_prediction_paths[2],
+                          logger)
+
     iteration = start_iter
     while True:
-        os.makedirs(os.path.dirname(prediction_paths[0].format(iteration)), exist_ok=True)
-        os.makedirs(os.path.dirname(test_prediction_paths[0].format(iteration)), exist_ok=True)
         os.makedirs(os.path.dirname(labeled_model_paths[0].format(iteration)), exist_ok=True)
         if iteration >= max_iter:
             break
@@ -113,14 +130,14 @@ def tri_training(labeled_path,
                                    'model_path': os.path.join(labeled_model_paths[i]
                                                               .format(iteration - 1), 'final_model'),
                                    'dataset_path': unlabeled_path,
-                                   'predictions_path': prediction_paths[i].format(iteration)
+                                   'predictions_path': prediction_paths[i]
                                })
             script = PREDICT_SCRIPT.format(config_path=TEMP_PREDICT_CONFIG_PATH)
             logger.info(f'Round #{iteration}: Predict on unlabeled data')
             subprocess.run(script, shell=True, check=True)
 
         # Stop when predictions from differs under a small ratio
-        agreement_ratio = global_agreement_ratio([p.format(iteration) for p in prediction_paths])
+        agreement_ratio = global_agreement_ratio([p for p in prediction_paths])
         logger.info(f'Round #{iteration}: Global agreement between 3 models: {agreement_ratio}')
         if agreement_ratio >= 0.9:
             logger.info(f'Round #{iteration}: Reach global agreement between 3 models')
@@ -131,24 +148,24 @@ def tri_training(labeled_path,
             for j in range(i+1, 3):
                 logger.info(f'Round #{iteration}: Select agreement between model {i} and {j}')
                 select_agreement(
-                    in_path1=prediction_paths[i].format(iteration),
-                    in_path2=prediction_paths[j].format(iteration),
-                    out_path=agreement_paths[sum(range(3))-(i+j)].format(iteration)
+                    in_path1=prediction_paths[i],
+                    in_path2=prediction_paths[j],
+                    out_path=agreement_paths[sum(range(3))-(i+j)]
                 )
 
         # Transfer
         for i in range(3):
             logger.info(f'Round #{iteration}: Transfer agreement to selection on model {i}')
             transfer_data(in_path1=labeled_path,
-                          in_path2=agreement_paths[i].format(iteration),
-                          out_path=selection_paths[i].format(iteration))
+                          in_path2=agreement_paths[i],
+                          out_path=selection_paths[i])
 
         for i in range(3):
             logger.info(f'Round #{iteration}: Retrain on model {i}')
             modify_config_file(DEFAULT_TRAIN_CONFIG_PATH,
                                TEMP_TRAIN_CONFIG_PATH,
                                {
-                                   'train_path': selection_paths[i].format(iteration),
+                                   'train_path': selection_paths[i],
                                    'save_path': labeled_model_paths[i].format(iteration),
                                    'log_path': train_log_paths[i].format(iteration)
                                })
@@ -165,15 +182,15 @@ def tri_training(labeled_path,
                                    'model_path': os.path.join(labeled_model_paths[i]
                                                               .format(iteration), 'final_model'),
                                    'dataset_path': DEFAULT_TEST_PATH,
-                                   'predictions_path': test_prediction_paths[i].format(iteration)
+                                   'predictions_path': test_prediction_paths[i]
                                })
             script = PREDICT_SCRIPT.format(config_path=TEMP_PREDICT_CONFIG_PATH)
             logger.info(f'Round #{iteration}: Predict on test data on model {i}')
             subprocess.run(script, shell=True, check=True)
         evaluate_tri_training(DEFAULT_TEST_PATH,
-                              test_prediction_paths[0].format(iteration),
-                              test_prediction_paths[1].format(iteration),
-                              test_prediction_paths[2].format(iteration),
+                              test_prediction_paths[0],
+                              test_prediction_paths[1],
+                              test_prediction_paths[2],
                               logger)
         iteration += 1
 
