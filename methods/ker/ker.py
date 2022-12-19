6778dcfc26e8bf5524d9e70432a3508ac8a99b5e
Trung Le <le.hg.trung@gmail.com>
2022-12-19 14:42:03 -0700

    start of ker frame work

--- methods/ker/ker.py
+++ methods/ker/ker.py
@@ -90,12 +90,12 @@ def ker(labeled_path,
         solve_all_docs(unlabeled_path=unlabeled_path,
                        atom_meta_path=atom_meta_path,
                        auto_meta_path=auto_meta_path,
-                       selection_path=prediction_path)
+                       selection_path=prediction_path + '.tmp')
 
         # Unify labeled and selected pseudo labeled data
         logger.info(f'Round #{iteration}: Unify labels and pseudo labels')
         transfer_data(in_path1=labeled_path,
-                      in_path2=prediction_path,
+                      in_path2=prediction_path + '.tmp',
                       out_path=selection_path)
 
         # Compute F1 on selection
