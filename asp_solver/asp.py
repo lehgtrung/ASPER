48cf7b3cd00b2be5ec4ace1437acaf270f2b4541
Trung Le <le.hg.trung@gmail.com>
2023-01-29 10:19:31 -0700

    debug

--- asp_solver/asp.py
+++ asp_solver/asp.py
@@ -207,6 +207,7 @@ def convert_doc_to_atoms(dataset, pred):
 
 
 def solve(command):
+    print(command)
     # print(command)
     # Write the program to a file
     process = subprocess.Popen(command,
